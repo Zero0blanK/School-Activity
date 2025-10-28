35a55efae9ea30b202b143c1da5caa701b011cdc
Mystyvyy <aprilbords123@gmail.com>
2025-10-28 20:05:33 +0800

    initial commit

---
+++ OOP.py
@@ -0,0 +1,19 @@
+class Dog:
+  def __init__(self, name, age):
+      self.name = name
+      self.age = age
+
+  def bark(self):
+      return f"Woof! Woof!"
+
+  def getInfo(self):
+      return f"Dog name: {self.name}, Age: {self.age} years old."
+  
+  def celebrateBirthday(self):
+      self.age += 1
+      return f"Happy Birthday! {self.name} is now {self.age} years old."
+  
+myDog = Dog("Happy", 5)
+print(myDog.bark())
+print(myDog.getInfo())
+print(myDog.celebrateBirthday())
